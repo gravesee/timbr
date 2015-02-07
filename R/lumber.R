ca7dcbd8c16e462897b895990251a8b1c859295e
Zelazny7 <gravcon5@gmail.com>
2015-02-07 17:10:27 -0600

    added index to lumberYard and subset function

--- R/lumber.R
+++ R/lumber.R
@@ -4,10 +4,38 @@
 # object that creates a store of processed timbr from a tree algorithm object
 #' @export
 lumberYard <- function(object, ...) {  
-  species <- species(object)  
+  species <- species(object)
+  # loop over object, convert trees, harvest them, and log the structure
+  
+  # l <- lapply(1:species$nTrees, function(i) harvest(timbr(object, i, species)))
+  idx <- list()
+  lumberList <- list()
+  for (n in 1:species$nTrees) {
+    x <- harvest(timbr(object, n, species))
+    idx[[n]] <- cbind(n, seq_along(x))
+    lumberList[[n]] <- x    
+  }  
+  
   structure(
-    lapply(1:species$nTrees, function(i) harvest(timbr(object, i, species))),
+    lumberList,
+    index = do.call(rbind, idx),
     class  = 'lumberYard')
+}
+
+# return a subset of nodes
+subsetLumberYard <- function(ly, i) {
+  
+  idx <- attr(ly, 'index')
+  
+  x <- lapply(i, function(x) {
+    ly[[idx[x, 1]]][[idx[x, 2]]]
+  })
+  
+  structure(
+    x,
+    nNodes = length(x),
+    termNodes = which(vapply(x, '[[', numeric(1), 'nodeStatus') == -1),
+    class = 'lumber')
 }
 
 # x is a list of nodes
